880e0be74773f2352f09db7f37956bc44cf688d5
Paramhans Singh <paramhanssingh084@gmail.com>
2023-08-28 23:07:44 +0530

    Added Props and React Components

---
+++ React Component-2/Projects/Anime-Pedia/src/AnimeList.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+// Complete the AnimeList Component
+class AnimeList extends Component {
+  render() {
+    return <div className="anime-list"></div>;
+  }
+}
+
+export default AnimeList;

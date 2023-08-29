85b2fdedb7fb25228ea4050bee898910a779e3ca
Paramhans Singh <paramhanssingh084@gmail.com>
2023-08-30 00:12:40 +0530

    Added Lifecycle Of Components

---
+++ Lecture-8 Lifecycle of Components/Projects/Photo Album/src/App.js
@@ -0,0 +1,55 @@
+import React from "react";
+import ImageList from "./components/ImageList";
+import "./styles.css";
+
+export default class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      images: [],
+      imageUrl: ""
+    };
+  }
+
+  // Function to add image to local storage
+  addImageToLS = () => {
+    localStorage.setItem(
+      "images",
+      JSON.stringify([...this.state.images, this.state.imageUrl])
+    );
+  };
+
+  // Function to get images from the local storage
+  getImagesFromLS = () => {
+    const images = localStorage.getItem("images");
+    if (!images) {
+      localStorage.setItem("images", []);
+      return [];
+    }
+    return JSON.parse(images);
+  };
+
+  onAddImage = (e) => {
+    e.preventDefault();
+  };
+
+  // Create function to set image url
+
+  // Use appropriate lifecycle method to get images
+
+  render() {
+    return (
+      <>
+        <form onSubmit={this.onAddImage}>
+          <input
+            type="text"
+            placeholder="Image URL"
+            value={this.state.imageUrl}
+          />
+          <button>Add Image</button>
+        </form>
+        <ImageList images={this.state.images} />
+      </>
+    );
+  }
+}
